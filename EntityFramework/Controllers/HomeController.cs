f04dd806ef9269881f061bc5d62368e9f29f67d0
k.ziarkowski <k.ziarkowski@PK3-3>
2019-10-03 18:45:27 +0200

    Add project files.

---
+++ EntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EntityFramework.Models;
+using EntityFramework.Data;
+
+namespace EntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Context db;
+        public HomeController(Context db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+
+            IQueryable<Car> model = db.Cars; // a to pobierze do pamieci sfiltrowanie dane najpierw sql potem dane Uzwyaj go na bazie dancyh
+            var x = model.ToList();
+            IEnumerable<Car> model2 = db.Cars.ToList(); // pobierze wszytskie do pamieci
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EntityFramework/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options): base(options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarModel> CarModels { get; set; }
+    }
+}
